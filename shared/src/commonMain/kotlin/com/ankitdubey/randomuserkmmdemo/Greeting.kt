d052fdda874bf17525343ef7fdaf19012b25f466
AnkitDubeyBol <ankit.dubey@lji.io>
2021-06-03 14:49:04 +0530

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/ankitdubey/randomuserkmmdemo/Greeting.kt
@@ -0,0 +1,7 @@
+package com.ankitdubey.randomuserkmmdemo
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/ankitdubey/randomuserkmmdemo/Platform.kt
@@ -0,0 +1,5 @@
+package com.ankitdubey.randomuserkmmdemo
+
+expect class Platform() {
+    val platform: String
+}
